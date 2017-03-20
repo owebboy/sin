bc5c274d3a10bdb308d51624b8cd99dad008f39e
Oliver Pope <opope@me.com>
2017-03-20 19:38:16 -0400

    finishing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function(value, mode) {
-  if (mode === 'deg' || mode == 'degs' || mode == 'degree' || mode == 'degrees') {
-    return new Number(Math.sin(Number(value)*(Math.PI/180)))
+  if (mode == 'deg' || mode == 'degs' || mode == 'degree' || mode == 'degrees') {
+    return new Number(Math.round(Math.sin(Number(value)*(Math.PI/180))))
   } else {
-    return new Number(Math.sin(Number(value)/Math.PI))
+    return new Number(Math.round(Math.sin(Number(value))))
   }
 }
--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 var sin = require('./index');
 var assert = require('assert');
 describe('sin', function() {
-  it('should return 0', function() {
-    assert.equal(0, sin(0));
-  });
+  it('0 radians should return 0', function() {
+    assert.equal(0, sin(0))
+  })
+  it('2pi radians should return 0', function() {
+    assert.equal(0, sin(2*Math.PI))
+  })
+  it('pi/2 radians should return 1', function() {
+    assert.equal(1, sin(Math.PI/2))
+  })
+  it('180 degrees should return 0', function() {
+    assert.equal(0, sin(180, 'deg'))
+  })
 });
